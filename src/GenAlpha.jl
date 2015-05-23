c0719095741c1afa640c18aab7db03c887f845a9
haoyio <haoyi@stanford.edu>
2015-05-22 23:26:02 -0700

    modified alpha generation script

--- src/GenAlpha.jl
+++ src/GenAlpha.jl
@@ -2,6 +2,7 @@ using Pairwise, Multiagent
 
 d = get_pomdp()
 lambda = float(ARGS[1])
+tag = int(ARGS[2])
 
 println("Starting QMDPs for lambda = ", lambda)
-gen_pairwise_policy(d, string("tula", lambdas[tag]), tag)
+gen_pairwise_policy(d, string("tula", tag), tag)
