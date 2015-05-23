cdc2cd1314db14a313a42f3bec2d90bc9ea2f68d
haoyio <haoyi@stanford.edu>
2015-05-22 23:04:41 -0700

    added console output

--- src/GenAlpha.jl
+++ src/GenAlpha.jl
@@ -3,7 +3,9 @@ using Pairwise, Multiagent
 d = get_pomdp()
 lambdas = logspace(-3, 2, 21)
 
+println("Starting QMDPs for lambda: ", lambdas)
 for tag = 1:length(lambdas)
+    println("lamba: ", lamdbas[tag])
     gen_pairwise_policy(d, lambdas[tag], tag)
 end # tag
 
