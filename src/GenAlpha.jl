6d9ed60b69de293da7ee87f7527d1610679e33b0
haoyio <haoyi@stanford.edu>
2015-05-22 23:05:21 -0700

    added console output

--- src/GenAlpha.jl
+++ src/GenAlpha.jl
@@ -5,7 +5,7 @@ lambdas = logspace(-3, 2, 21)
 
 println("Starting QMDPs for lambda: ", lambdas)
 for tag = 1:length(lambdas)
-    println("lamba: ", lamdbas[tag])
+    println("lamba: ", lambdas[tag])
     gen_pairwise_policy(d, lambdas[tag], tag)
 end # tag
 
