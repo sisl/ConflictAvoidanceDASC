4ca3f2c4f4767cad4cd464247665eb9af6c61e49
haoyio <haoyi@stanford.edu>
2015-06-22 11:18:23 -0700

    removed test files but keep local

--- src/GenAlpha.jl
+++
@@ -1,8 +0,0 @@
-using Pairwise, Multiagent
-
-d = get_pomdp()
-lambda = float(ARGS[1])
-tag = int(ARGS[2])
-
-println("Starting QMDPs for lambda = ", lambda)
-gen_pairwise_policy(d, lambda, string("tula", tag))
