b3d47ca73515d4b1d44c5f8e3259946890c25368
haoyio <haoyi@stanford.edu>
2015-08-07 02:10:13 -0700

    made script for parsca

---
+++ parallel/ParSCA.jl
@@ -0,0 +1,45 @@
+# load modules
+push!(LOAD_PATH, "./dvi/src")
+
+addprocs(int(CPU_CORES / 2))
+
+using DiscreteValueIteration
+using SCAs, SCAIterators, SCAConst
+using GridInterpolations
+
+mdp = SCA()
+
+# check size of mdp
+function getBytes(x)
+   total = 0;
+   fieldNames = typeof(x).names;
+   if fieldNames == ()
+      return sizeof(x);
+   else
+     for fieldName in fieldNames
+        total += getBytes(getfield(x,fieldName));
+     end
+     return total;
+   end
+end
+
+println("mdp of type ", typeof(mdp), " takes up ", getBytes(mdp) / 1000.0, " kB")
+
+# parallel solution
+numProcs = int(CPU_CORES / 2)
+solver = ParallelSolver(
+    numProcs,
+    maxIterations = 1000,
+    tolerance = 1e-6,
+    gaussSiedel = false,
+    includeV = true,
+    includeQ = true,
+    includeA = true)
+
+policy = solve(solver, mdp, verbose = true)
+
+# save solution
+using HDF5, JLD
+
+solQ = policy.Q'
+save("../data/par-alpha.jld", "solQ", solQ)
